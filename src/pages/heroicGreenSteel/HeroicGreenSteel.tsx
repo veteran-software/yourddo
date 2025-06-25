5a12cdee1817d53b4ffd9627317dea7d82099b0e
Keith Russo <keith.russo@veteransoftware.us>
2025-06-24 22:01:30 -0400

    Add link to Green Steel issue tracker in HeroicGreenSteel.tsx
    - Import `FaArrowUpRightFromSquare` from `react-icons/fa6`.
    - Add link to GitHub issues for Green Steel bugs in the card header.
    - Adjust `Card.Header` styles by adding padding (`p-1`).

--- src/pages/heroicGreenSteel/HeroicGreenSteel.tsx
+++ src/pages/heroicGreenSteel/HeroicGreenSteel.tsx
@@ -1,4 +1,5 @@
 import { Card, Col, Container, Row } from 'react-bootstrap'
+import { FaArrowUpRightFromSquare } from 'react-icons/fa6'
 import { shallowEqual } from 'react-redux'
 import { useAppSelector } from '../../redux/hooks.ts'
 import DevastationBasicDropdown from './components/DevastationBasicDropdown.tsx'
@@ -15,8 +16,18 @@ const HeroicGreenSteel = () => {
   return (
     <Container className='px-0'>
       <Card>
-        <Card.Header className='text-center'>
+        <Card.Header className='text-center p-1'>
           <h4 className='mb-0'>Heroic Green Steel Crafting</h4>
+          <small>
+            <a
+              href='https://github.com/veteran-software/yourddo/issues?q=is%3Aissue%20state%3Aopen%20label%3A%22Green%20Steel%22'
+              target='_blank'
+              rel='noreferrer'
+              title='Green Steel Known Issues & Bug Reports'
+            >
+              Known Issues / Bug Reports <FaArrowUpRightFromSquare size={10} />
+            </a>
+          </small>
         </Card.Header>
 
         <Card.Body>
