3649e6587410f3573f6cd9f63c1458bf12bf6dec
Keith Russo <keith.russo@veteransoftware.us>
2025-06-27 15:44:57 -0400

    Refactor IngredientList and add reusable TierAccordionItem
    - Refactor `IngredientList` to use `TierAccordionItem` component.
    - Add new `TierAccordionItem.tsx` for tiered ingredient display logic.
    - Create `DualShardInstructions.tsx` for specific ingredient guidance.
    - Remove redundant code in `IngredientList` and optimize material checks.
    - Update imports in `IngredientList` to include `TierAccordionItem`.

--- src/pages/heroicGreenSteel/components/lists/IngredientList.tsx
+++ src/pages/heroicGreenSteel/components/lists/IngredientList.tsx
@@ -1,9 +1,7 @@
 import { Accordion, Card, ListGroup, Stack } from 'react-bootstrap'
-import { FaExclamationTriangle } from 'react-icons/fa'
 import { shallowEqual } from 'react-redux'
 import CraftedIngredientDisplay from '../../../../components/CraftedIngredientDisplay.tsx'
 import FarmedIngredientDisplay from '../../../../components/FarmedIngredientDisplay.tsx'
-import { altarOfDevastation } from '../../../../data/altarOfDevastation.ts'
 import { ingredients } from '../../../../data/ingredients.ts'
 import { useAppSelector } from '../../../../redux/hooks.ts'
 import type { CraftingIngredient } from '../../../../types/crafting.ts'
@@ -11,6 +9,7 @@ import type { Ingredient } from '../../../../types/ingredients.ts'
 import { findIngredientByName } from '../../../../utils/objectUtils.ts'
 import { allAltars } from '../../helpers/elementalData.ts'
 import useRecipeBuilder from '../../hooks/useRecipeBuilder.ts'
+import TierAccordionItem from '../TierAccordionItem.tsx'
 import FecundityList from './FecundityList.tsx'
 import MaterialsAccordion from './MaterialsAccordion.tsx'
 
@@ -46,9 +45,11 @@ const IngredientList = () => {
     return <></>
   }
 
+  const hasMaterials = Object.entries(rawMaterials).length > 0 || Object.entries(craftedMaterials).length > 0
+
   return (
     <Stack direction='vertical' gap={3} className='m-0 p-0'>
-      {(Object.entries(rawMaterials).length > 0 || Object.entries(craftedMaterials).length > 0) && (
+      {hasMaterials && (
         <Card>
           <Card.Header className='text-center'>
             <h6 className='mb-0'>Full Ingredient List</h6>
@@ -67,111 +68,38 @@ const IngredientList = () => {
           <Accordion alwaysOpen={false} className='rounded-0'>
             <FecundityList callback={getDisplay} />
 
-            {selectedInvasionItem && (
-              <Accordion.Item eventKey='3' className='rounded-0'>
-                <Accordion.Header>[Tier: 1] : {selectedInvasionItem.name}</Accordion.Header>
-                <Accordion.Body className='p-0'>
-                  <ListGroup variant='flush'>
-                    <ListGroup.Item variant='secondary' className='text-center'>
-                      <small>
-                        Combine the following ingredients in the <strong>Altar of Invasion</strong>.<br />
-                        Hover or tap on the ingredient image to learn more about each ingredient.
-                      </small>
-                    </ListGroup.Item>
-                    {selectedInvasionItem.requirements.map((ingredient: CraftingIngredient) => getDisplay(ingredient))}
-                  </ListGroup>
-                </Accordion.Body>
-              </Accordion.Item>
-            )}
+            <TierAccordionItem
+              eventKey='3'
+              tier='Tier: 1'
+              altarName='Altar of Invasion'
+              selectedItem={selectedInvasionItem}
+              getDisplay={getDisplay}
+            />
 
-            {selectedSubjugationItem && (
-              <Accordion.Item eventKey='4' className='rounded-0'>
-                <Accordion.Header>[Tier: 2] : {selectedSubjugationItem.name}</Accordion.Header>
-                <Accordion.Body className='p-0'>
-                  <ListGroup variant='flush'>
-                    <ListGroup.Item variant='secondary' className='text-center'>
-                      <small>
-                        Combine the following ingredients in the <strong>Altar of Subjugation</strong>.<br />
-                        Hover or tap on the ingredient image to learn more about each ingredient.
-                      </small>
-                    </ListGroup.Item>
-                    {selectedSubjugationItem.requirements.map((ingredient: CraftingIngredient) =>
-                      getDisplay(ingredient)
-                    )}
-                  </ListGroup>
-                </Accordion.Body>
-              </Accordion.Item>
-            )}
+            <TierAccordionItem
+              eventKey='4'
+              tier='Tier: 2'
+              altarName='Altar of Subjugation'
+              selectedItem={selectedSubjugationItem}
+              getDisplay={getDisplay}
+            />
 
-            {selectedDevastationBasic && (
-              <Accordion.Item eventKey='6' className='rounded-0'>
-                <Accordion.Header>[Tier: 3 Basic] : {selectedDevastationBasic.name}</Accordion.Header>
-                <Accordion.Body className='p-0'>
-                  <ListGroup variant='flush'>
-                    <ListGroup.Item variant='secondary' className='text-center'>
-                      <small>
-                        Combine the following ingredients in the <strong>Altar of Devastation</strong>.<br />
-                        Hover or tap on the ingredient image to learn more about each ingredient.
-                      </small>
-                    </ListGroup.Item>
-                    {selectedDevastationBasic.requirements.map((ingredient: CraftingIngredient) =>
-                      getDisplay(ingredient)
-                    )}
-                  </ListGroup>
-                </Accordion.Body>
-              </Accordion.Item>
-            )}
+            <TierAccordionItem
+              eventKey='6'
+              tier='Tier: 3 Basic'
+              altarName='Altar of Devastation'
+              selectedItem={selectedDevastationBasic}
+              getDisplay={getDisplay}
+            />
 
-            {selectedDevastationFocused && (
-              <Accordion.Item eventKey='7' className='rounded-0'>
-                <Accordion.Header>
-                  {selectedDevastationFocused.requirements[1].name.includes('Dual Shard') && (
-                    <span className='align-bottom'>
-                      <FaExclamationTriangle color='yellow' />
-                      &nbsp;
-                    </span>
-                  )}
-                  [Tier: 3 Focused] : {selectedDevastationFocused.name}
-                </Accordion.Header>
-                <Accordion.Body className='p-0'>
-                  <ListGroup variant='flush'>
-                    {selectedDevastationFocused.requirements.some((selected: CraftingIngredient) =>
-                      selected.name.includes('Dual Shard')
-                    ) && (
-                      <ListGroup.Item variant='info'>
-                        <span className='text-decoration-underline'>
-                          To craft the <strong>{selectedDevastationFocused.requirements[1].name}</strong>:
-                        </span>
-                        <ol>
-                          <li>
-                            Create a(n){' '}
-                            {
-                              findIngredientByName(selectedDevastationFocused.requirements[1].name, altarOfDevastation)
-                                ?.requirements[0].name
-                            }
-                          </li>
-                          <li>
-                            Create a(n){' '}
-                            {
-                              findIngredientByName(selectedDevastationFocused.requirements[1].name, altarOfDevastation)
-                                ?.requirements[1].name
-                            }
-                          </li>
-                          <li>
-                            Combine both in the <strong>Altar of Devastation</strong> with a{' '}
-                            <strong>Shavarath High Energy Cell</strong>
-                          </li>
-                        </ol>
-                      </ListGroup.Item>
-                    )}
-
-                    {selectedDevastationFocused.requirements.map((ingredient: CraftingIngredient) =>
-                      getDisplay(ingredient)
-                    )}
-                  </ListGroup>
-                </Accordion.Body>
-              </Accordion.Item>
-            )}
+            <TierAccordionItem
+              eventKey='7'
+              tier='Tier: 3 Focused'
+              altarName='Altar of Devastation'
+              selectedItem={selectedDevastationFocused}
+              getDisplay={getDisplay}
+              showDualShardWarning={true}
+            />
           </Accordion>
         </Card.Body>
       </Card>
