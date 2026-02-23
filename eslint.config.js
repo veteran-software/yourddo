6e4a5ef067ca530a50d6803ac93d508a28dbe93c
Keith Russo <keith.russo@veteransoftware.us>
2026-02-23 09:10:37 -0500

    Update ESLint configuration to disable no-unnecessary-condition rule
    - Disabled `@typescript-eslint/no-unnecessary-condition` rule in `eslint.config.js`.

--- eslint.config.js
+++ eslint.config.js
@@ -31,7 +31,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unnecessary-condition': 'off'
     }
   }
 )
