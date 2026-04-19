442093a8dfa92c1a7af27f846401b82835c7fa4a
Keith Russo <keith.russo@veteransoftware.us>
2026-04-18 20:12:21 -0400

    Increase retry loop delay in API
    - **api.go**: Updated sleep duration from 400ms to 1000ms in retry loop.

--- tools/dataSpider/api/api.go
+++ tools/dataSpider/api/api.go
@@ -144,7 +144,7 @@ func FetchCategoryContent(categoryName string) (map[string]string, error) {
 			break
 		}
 
-        time.Sleep(400 * time.Millisecond)
+        time.Sleep(1000 * time.Millisecond)
 	}
 
 	return contentMap, nil
